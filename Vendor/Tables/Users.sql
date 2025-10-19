1fdea261919af0056f07d6c33473a3760ed06dc8
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-10-20 00:22:44 +0530

    set references

--- Vendor/Tables/Users.sql
+++ Vendor/Tables/Users.sql
@@ -1,13 +1,16 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [UserID]    UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
-    [FullName]  NVARCHAR (100)   NOT NULL,
-    [Username]  NVARCHAR (50)    NOT NULL,
-    [Email]     NVARCHAR (100)   NOT NULL,
-    [Password]  NVARCHAR (256)   NOT NULL,
-    [CreatedAt] DATETIME         DEFAULT (getdate()) NULL,
-    [UpdatedAt] DATETIME         NULL,
+    [UserID]    UNIQUEIDENTIFIER DEFAULT (NEWID()) NOT NULL,
+    [VendorID]  UNIQUEIDENTIFIER NOT NULL, -- link to Vendors
+    [FullName]  NVARCHAR(100)   NOT NULL,
+    [Username]  NVARCHAR(50)    NOT NULL,
+    [Email]     NVARCHAR(100)   NOT NULL,
+    [Password]  NVARCHAR(256)   NOT NULL,
+    [CreatedAt] DATETIME        DEFAULT (GETDATE()) NULL,
+    [UpdatedAt] DATETIME        NULL,
     PRIMARY KEY CLUSTERED ([UserID] ASC),
     UNIQUE NONCLUSTERED ([Email] ASC),
-    UNIQUE NONCLUSTERED ([Username] ASC)
+    UNIQUE NONCLUSTERED ([Username] ASC),
+    CONSTRAINT FK_Users_Vendors FOREIGN KEY ([VendorID]) 
+        REFERENCES [dbo].[Vendors]([VendorID])
+        ON DELETE CASCADE  -- optional, deletes users if vendor is deleted
 );
-
