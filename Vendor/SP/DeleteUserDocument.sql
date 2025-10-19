1d97386f5d29d4e26e56cf33ccf0775526b7d3d2
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-10-20 01:48:32 +0530

    ADD AND UpdateUserDocument.sql

---
+++ Vendor/SP/DeleteUserDocument.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[DeleteUserDocument]
+    @UserID UNIQUEIDENTIFIER,
+    @DocumentType NVARCHAR(50)  -- e.g., 'ProfilePicture', 'IDProof', 'Other'
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        -- Capture the document to be deleted
+        DECLARE @DeletedDoc TABLE (
+            DocumentID UNIQUEIDENTIFIER,
+            UserID UNIQUEIDENTIFIER,
+            DocumentType NVARCHAR(50),
+            DocumentURL NVARCHAR(500),
+            UploadedAt DATETIME,
+            UpdatedAt DATETIME
+        );
+
+        INSERT INTO @DeletedDoc
+        SELECT *
+        FROM [dbo].[UserDocuments]
+        WHERE UserID = @UserID
+          AND DocumentType = @DocumentType;
+
+        -- Delete the document
+        DELETE FROM [dbo].[UserDocuments]
+        WHERE UserID = @UserID
+          AND DocumentType = @DocumentType;
+
+        -- Optional: return deleted document info
+        SELECT * FROM @DeletedDoc;
+
+    END TRY
+    BEGIN CATCH
+        THROW;
+    END CATCH
+END
--- Vendor/SP/UpdateUserDocument.sql
+++ Vendor/SP/UpdateUserDocument.sql
@@ -1,26 +1,46 @@
 ﻿CREATE PROCEDURE [dbo].[UpdateUserDocument]
     @UserID UNIQUEIDENTIFIER,
-    @DocumentType NVARCHAR(50),    -- e.g., 'ProfilePicture', 'IDProof', 'Other'
+    @DocumentType NVARCHAR(50),
     @NewDocumentURL NVARCHAR(500)
 AS
 BEGIN
     SET NOCOUNT ON;
 
     BEGIN TRY
-        -- Update the document URL and set UpdatedAt
-        UPDATE [dbo].[UserDocuments]
-        SET 
-            DocumentURL = @NewDocumentURL,
-            UpdatedAt = GETDATE()
-        WHERE UserID = @UserID
-          AND DocumentType = @DocumentType;
+        IF EXISTS (
+            SELECT 1 
+            FROM [dbo].[UserDocuments]
+            WHERE [UserID] = @UserID AND [DocumentType] = @DocumentType
+        )
+        BEGIN
+            UPDATE [dbo].[UserDocuments]
+            SET 
+                [DocumentURL] = @NewDocumentURL,
+                [UpdatedAt] = GETDATE()
+            WHERE 
+                [UserID] = @UserID
+                AND [DocumentType] = @DocumentType;
+        END
+        ELSE
+        BEGIN
+            INSERT INTO [dbo].[UserDocuments] (
+                [UserID], [DocumentType], [DocumentURL]
+            )
+            VALUES (
+                @UserID, @DocumentType, @NewDocumentURL
+            );
+        END
 
-        -- Return the updated document info
-        SELECT *
+        -- Return the updated/inserted record
+        SELECT 
+            [DocumentID],
+            [UserID],
+            [DocumentType],
+            [DocumentURL],
+            [UploadedAt],
+            [UpdatedAt]
         FROM [dbo].[UserDocuments]
-        WHERE UserID = @UserID
-          AND DocumentType = @DocumentType;
-
+        WHERE [UserID] = @UserID AND [DocumentType] = @DocumentType;
     END TRY
     BEGIN CATCH
         THROW;
