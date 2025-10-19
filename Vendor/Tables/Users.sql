2b8e7a300d586c5e71c0e4ff25060c3b47687d91
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-10-20 00:18:11 +0530

    Users.sql Vendor/Tables/Users.sql

---
+++ Vendor/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]    UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [FullName]  NVARCHAR (100)   NOT NULL,
+    [Username]  NVARCHAR (50)    NOT NULL,
+    [Email]     NVARCHAR (100)   NOT NULL,
+    [Password]  NVARCHAR (256)   NOT NULL,
+    [CreatedAt] DATETIME         DEFAULT (getdate()) NULL,
+    [UpdatedAt] DATETIME         NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
---
+++ Vendor/Tables/Vendors.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Vendors] (
+    [VendorID]     UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [CompanyName]  NVARCHAR (100)   NOT NULL,
+    [ContactName]  NVARCHAR (100)   NOT NULL,
+    [ContactEmail] NVARCHAR (100)   NOT NULL,
+    [ContactPhone] NVARCHAR (20)    NULL,
+    [Address]      NVARCHAR (200)   NULL,
+    [City]         NVARCHAR (50)    NULL,
+    [State]        NVARCHAR (50)    NULL,
+    [Country]      NVARCHAR (50)    NULL,
+    [GSTNumber]    NVARCHAR (20)    NULL,
+    [PANNumber]    NVARCHAR (20)    NULL,
+    [CreatedAt]    DATETIME         DEFAULT (getdate()) NULL,
+    [UpdatedAt]    DATETIME         NULL,
+    PRIMARY KEY CLUSTERED ([VendorID] ASC)
+);
+
