0aabb0ac4eb7e9303e28c6c2b7e8ce0b58b46cb2
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-10-20 01:09:33 +0530

    update and DeleteUser.sql

---
+++ Vendor/SP/DeleteUser.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[DeleteUser]
+    @UserID UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        -- Optional: capture the user before deletion
+        DECLARE @DeletedUser TABLE (
+            UserID UNIQUEIDENTIFIER,
+            VendorID UNIQUEIDENTIFIER,
+            FullName NVARCHAR(100),
+            Username NVARCHAR(50),
+            Email NVARCHAR(100),
+            Password NVARCHAR(256),
+            CreatedAt DATETIME,
+            UpdatedAt DATETIME
+        );
+
+        INSERT INTO @DeletedUser
+        SELECT *
+        FROM [dbo].[Users]
+        WHERE UserID = @UserID;
+
+        -- Delete the user
+        DELETE FROM [dbo].[Users]
+        WHERE UserID = @UserID;
+
+        -- Return deleted user info
+        SELECT * FROM @DeletedUser;
+
+    END TRY
+    BEGIN CATCH
+        THROW;
+    END CATCH
+END
---
+++ Vendor/SP/UpdateUser.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[UpdateUser]
+    @UserID UNIQUEIDENTIFIER,
+    @FullName NVARCHAR(100) = NULL,
+    @Username NVARCHAR(50) = NULL,
+    @Email NVARCHAR(100) = NULL,
+    @Password NVARCHAR(100) = NULL  -- plain text password
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        -- Hash password if provided
+        DECLARE @HashedPassword VARBINARY(256) = NULL;
+        IF @Password IS NOT NULL
+        BEGIN
+            SET @HashedPassword = HASHBYTES('SHA2_256', CONVERT(VARBINARY(MAX), @Password));
+        END
+
+        -- Update the user
+        UPDATE [dbo].[Users]
+        SET
+            FullName = COALESCE(@FullName, FullName),
+            Username = COALESCE(@Username, Username),
+            Email = COALESCE(@Email, Email),
+            Password = COALESCE(@HashedPassword, Password),
+            UpdatedAt = GETDATE()
+        WHERE UserID = @UserID;
+
+        -- Return updated user
+        SELECT *
+        FROM [dbo].[Users]
+        WHERE UserID = @UserID;
+
+    END TRY
+    BEGIN CATCH
+        THROW;
+    END CATCH
+END
